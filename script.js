f41609f3c9823c0c96cd178f4410555dfe317dc3
mo speight <you@example.com>
2023-12-07 02:30:59 -0500

    Adds async event listener  for start button

--- script.js
+++ script.js
@@ -7,13 +7,22 @@ async function selectMediaStream() {
        const mediaStream = await navigator.mediaDevices.getDisplayMedia();
        videoElement.srcObject = mediaStream;
        videoElement.onloadedmetadata = () => {
-          videoElement.onplay();
+          videoElement.play();
        }
    } catch (error) {
     //Catch Error Here
     console.log('whoops, error here:', error)
    }
 };
+
+button.addEventListener('click', async () => {
+   //Disable Button
+   button.disabled = true;
+   //Start Picture in Picture
+   await videoElement.requestPictureInPicture();
+   //Reset Button
+   button.disabled = false;
+});
 
 //On Load
 selectMediaStream();
