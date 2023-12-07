d2ecd9c662d30a956a5466e451448f75e6e247c4
mo speight <you@example.com>
2023-12-07 01:28:43 -0500

    implements mediaStream

--- script.js
+++ script.js
@@ -4,7 +4,11 @@ const button = document.getElementById('button');
 // Prompt the to select media stream, pass to video element, then play
 async function selectMediaStream() {
    try {
-       
+       const mediaStream = await navigator.mediaDevices.getDisplayMedia();
+       videoElement.srcObject = mediaStream;
+       videoElement.onloadedmetadata = () => {
+          videoElement.onplay();
+       }
    } catch (error) {
     //Catch Error Here
     console.log('whoops, error here:', error)
